b9021375a90f524e79ec6f6bc5758cd6a1084cff
sc <sc@scdeMacBook-Pro.local>
2017-08-11 13:18:36 +0800

    初始化

---
+++ classes/WWFish.h
@@ -0,0 +1,13 @@
+//
+//  WWFish.h
+//  shuangChuang
+//
+//  Created by sc on 2017/8/11.
+//  Copyright © 2017年 魏国清. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WWFish : UIView
+
+@end
---
+++ classes/WWFish.m
@@ -0,0 +1,21 @@
+//
+//  WWFish.m
+//  shuangChuang
+//
+//  Created by sc on 2017/8/11.
+//  Copyright © 2017年 魏国清. All rights reserved.
+//
+
+#import "WWFish.h"
+
+@implementation WWFish
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
